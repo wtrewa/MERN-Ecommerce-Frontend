63bc4615c01383a638ec19bed166802b40ca6dea
wtrewa <psaurabh574@gmail.com>
2023-12-20 17:23:19 +0530

    cartPage completed

---
+++ src/Pages/CartPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from '../features/Cart/Cart'
+
+const CartPage = () => {
+  return (
+    <div>
+      <Cart>
+        
+      </Cart>
+    </div>
+  )
+}
+
+export default CartPage
--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -8,6 +8,7 @@ import {
   ShoppingCartIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const user = {
   name: "Tom Cook",
@@ -71,15 +72,19 @@ const Navbar = ({ children }) => {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
+                      <Link to={'/cart'}>
                       <button
+                      
                         type="button"
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
+                        
                         <ShoppingCartIcon
                           className="h-6 w-6"
                           aria-hidden="true"
                         />
                       </button>
+                      </Link>
                       <span className="inline-flex items-center rounded-md  mb-7 -ml-2  bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                         3
                       </span>
@@ -183,6 +188,7 @@ const Navbar = ({ children }) => {
                         {user.email}
                       </div>
                     </div>
+                    <Link to={'/cart'}>
                     <button
                       type="button"
                       className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -192,6 +198,7 @@ const Navbar = ({ children }) => {
                         aria-hidden="true"
                       />
                     </button>
+                    </Link>
                       <span className="inline-flex items-center rounded-md  mb-7 -ml-2  bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                         3
                       </span>
